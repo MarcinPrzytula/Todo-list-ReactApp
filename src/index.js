66aea3d4a949c8f7c64ac0d987083fc7e6cf139c
Marcin <mprzytula94@gmail.com>
2021-09-30 07:51:51 +0200

    changing the structure of files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './my_toDo/App.js';
-// // import App from './kurs_toDo/App.js';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './my_toDo_redux/App.js';
-// import App from './redux/App.js';
-// import App from './kurs_toDo/App.js';
+
+import App from './App.js';
 
 ReactDOM.render(
   <App />,
